e73ae6ea819530d689436c85171a77367cf38d5f
Nima Reza <NimaaRezaa99@gmail.com>
2024-09-19 22:54:34 +0330

    list creator and date calculator created

---
+++ src/js/index.js
@@ -0,0 +1,78 @@
+let allPaymentData = [];
+const API_URL = "http://localhost:3000/transactions";
+
+const getDataBtn = document.querySelector(".btn__show-transactions");
+const transactionsUL = document.querySelector(".transactions");
+const transactionsListHeader = `
+
+        <li class="transactions__header">
+          <p class="num">
+            ردیف
+          </p>
+          <p class="transaction__type">
+            نوع تراکنش
+          </p>
+          <p class="transaction__price">
+            مبلغ
+          </p>
+          <p class="transaction__number">
+            شماره پیگیری
+          </p>
+          <p class="transaction__date">
+            تاریخ تراکنش
+          </p>
+        </li>`;
+
+const dateFormatter = (date) => {
+
+        const recivedDate = new Date(date);
+        const year = recivedDate.getFullYear();
+        const month = recivedDate.getMonth() + 1;
+        const day = recivedDate.getDay() + 1;
+        const fullDate = `${year}/${month}/${day}`;
+        return fullDate;
+}
+
+const liElementCreator = (data) => {
+
+    data.forEach(element => {
+
+    const fullDate = dateFormatter(element.date);
+    const newLi = document.createElement('li');
+    newLi.innerHTML = `
+    <p class="num">
+             ${element.id}
+           </p>
+           <p class="transaction__type">
+            ${element.type}
+           </p>
+           <p class="transaction__price">
+             ${element.price}
+           </p>
+           <p class="transaction__number">
+             ${element.refId}
+           </p>
+           <p class="transaction__date">
+           ${fullDate}
+           </p>
+    `
+
+    newLi.classList.add('transactions__header', 'transactions__item')
+    transactionsUL.appendChild(newLi);
+
+    });
+    
+}
+
+function getData() {
+
+    transactionsUL.innerHTML = transactionsListHeader;
+  axios
+    .get(API_URL)
+    .then((res) => {
+     liElementCreator(res.data)
+      });
+
+}
+
+getDataBtn.addEventListener("click", getData);
