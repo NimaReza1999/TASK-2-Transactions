db5fead90a28bef2d9ea05c1712b398cbc17530c
Nima Reza <NimaaRezaa99@gmail.com>
2024-09-20 18:16:51 +0330

    dateformatter has been added

--- src/js/index.js
+++ src/js/index.js
@@ -30,40 +30,53 @@ const transactionsListHeader = `
 const getDataBtn = document.querySelector(".btn__show-transactions");
 const transactionsUL = document.querySelector(".transactions");
 const searchInput = document.querySelector(".search__input");
+const filterRadios = document.querySelectorAll(".filter__radio")
+const radioListToFilter = [...filterRadios];
 
-
+ 
 
 // *Functions
 
 
 const dateFormatter = (date) => {
 
+  const formatter = new Intl.DateTimeFormat('fa-IR', {
+    calender:'persian',
+    year:'numeric',
+    month:'numeric',
+    day:'numeric'
+  })
+
+      
         const recivedDate = new Date(date);
-        const year = recivedDate.getFullYear();
-        const month = recivedDate.getMonth() + 1;
-        const day = recivedDate.getDay() + 1;
-        const fullDate = `${year}/${month}/${day}`;
-        return fullDate;
+        const hour = recivedDate.getHours();
+        const minutes = recivedDate.getMinutes();
+        const persianHour = new Intl.NumberFormat('fa-IR').format(hour);
+        const persianMinute = new Intl.NumberFormat('fa-IR').format(minutes);
+        const finalDate = formatter.format(recivedDate);
+        return (`${finalDate} در ساعت ${persianHour}:${persianMinute}`)
 }
 
 const liElementCreator = (data) => {
 
     data.forEach(element => {
 
+    const refIdString = `${element.refId}`;
+    console.log(typeof(refIdString))
     const fullDate = dateFormatter(element.date);
     const newLi = document.createElement('li');
     newLi.innerHTML = `
     <p class="num">
-             ${element.id}
+             ${new Intl.NumberFormat('fa-IR').format(element.id)}
            </p>
            <p class="transaction__type">
             ${element.type}
            </p>
            <p class="transaction__price">
-             ${element.price}
+             ${new Intl.NumberFormat('fa-IR').format(element.price)}
            </p>
            <p class="transaction__number">
-             ${element.refId}
+             ${new Intl.NumberFormat('fa-IR').format(refIdString).replace(/,/g, '')}
            </p>
            <p class="transaction__date">
            ${fullDate}
@@ -79,7 +92,8 @@ const liElementCreator = (data) => {
 
 function getData() {
 
-    transactionsUL.innerHTML = transactionsListHeader;
+  transactionsUL.innerHTML = transactionsListHeader;
+  filterRadios[0].setAttribute('checked', 'checked');
   axios
     .get(API_URL)
     .then((res) => {
@@ -99,10 +113,67 @@ const searchHandler = () => {
 const searchItemShower = (data) => {
 
     transactionsUL.innerHTML = transactionsListHeader;
-    liElementCreator(data)
+    data.forEach(element => {
+
+      const fullDate = dateFormatter(element.date);
+      const newLi = document.createElement('li');
+      newLi.innerHTML = `
+      <p class="num">
+               ${data.indexOf(element) + 1}
+             </p>
+             <p class="transaction__type">
+              ${element.type}
+             </p>
+             <p class="transaction__price">
+               ${element.price}
+             </p>
+             <p class="transaction__number">
+               ${element.refId}
+             </p>
+             <p class="transaction__date">
+             ${fullDate}
+             </p>
+      `
+  
+      newLi.classList.add('transactions__header', 'transactions__item')
+      transactionsUL.appendChild(newLi);
+  
+      });
+      
+}
+
+const filterHandler = (event) => {
+
+  const filter = event.target.id;
+  
+  switch (filter) {
+
+    case "high_to_low":
+      axios
+      .get(`${API_URL}?sort=price&_order=asc`)
+      .then((res)=>console.log(res.data))
+      break;
+
+
+    case "low_to_high":
+      console.log("lower");
+      break;
+
+
+    case "newest":
+      console.log("new");
+      break;
+
+
+    default:
+      console.log("all");
+  }
 }
 
 // *EeventListeners
 
 getDataBtn.addEventListener("click", getData);
 searchInput.addEventListener("input", searchHandler);
+radioListToFilter.forEach((item) => {
+  item.addEventListener("click", filterHandler)
+})
